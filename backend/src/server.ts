639a03ce11c409e165062b50a0eac8a451274ae7
YhAbH <yahir.abraham.h@gmail.com>
2025-11-06 18:23:05 -0600

    main

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,17 @@
 import express from "express";
-import colors from "colors";
+import colors, { blue } from "colors";
 import morgan from "morgan";
+import { db } from "./config/db";
+
+async function connectDB() {
+  try {
+    await db.authenticate();
+    await db.sync();
+    console.log(colors.blue.bold("Conexión exitosa con la DB"));
+  } catch (error) {
+    console.log(colors.red.bold("Error al conectar con la DB:"), error);
+  }
+}
 
 const app = express();
 
@@ -8,4 +19,4 @@ app.use(morgan("dev"));
 
 app.use(express.json());
 
-export default app;
+export default connectDB;
